196310a4002d27113321013fd2fb8a3a89064243
Francis Phiri <franzparks@gmail.com>
2017-07-23 21:29:10 -0700

    add code lookupservice implementation

---
+++ LookupMicroService/src/main/java/com/insurance/services/imp/CodeLookupServiceImp.java
@@ -0,0 +1,92 @@
+package com.insurance.services.imp;
+
+import org.springframework.stereotype.Service;
+
+import com.insurance.domain.InsuranceCodeContainer;
+import com.insurance.services.CodeLookupService;
+/**
+ * @author francisphiri
+ *
+ */
+@Service
+public class CodeLookupServiceImp implements CodeLookupService {
+	
+	@Override
+	public InsuranceCodeContainer findByISODescription(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByNAICS(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByGeneralDescription(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByGeneralDescriptionContains(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByGeneralDescriptionIgnoreCase(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByNCCI(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByCaWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByDeWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByMiWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByNyWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByNjWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByPaWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+	@Override
+	public InsuranceCodeContainer findByTxWC(String text) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+	
+}
