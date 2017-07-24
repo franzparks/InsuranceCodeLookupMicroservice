d2fc86ca3d88a164e21d61d0cc7ded69b3e1778e
Francis Phiri <franzparks@gmail.com>
2017-07-23 18:56:01 -0700

    add initial files

---
+++ LookupMicroService/src/main/java/com/insurance/LookupMicroServiceApplication.java
@@ -0,0 +1,12 @@
+package com.insurance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LookupMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LookupMicroServiceApplication.class, args);
+	}
+}
