985b73e5fa3b7bde824242262f3cbb6578ffdbeb
Francis Phiri <franzparks@gmail.com>
2017-07-23 19:48:01 -0700

    clean up

--- LookupMicroService/src/main/java/com/insurance/LookupMicroServiceApplicationServer.java
+++ LookupMicroService/src/main/java/com/insurance/LookupMicroServiceApplicationServer.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@SpringBootApplication
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 public class LookupMicroServiceApplicationServer {
