225915c36146d4eed6f03b4604d6e47d81fd800e
Francis Phiri <franzparks@gmail.com>
2017-07-23 21:27:02 -0700

    add code lookupservice

---
+++ LookupMicroService/src/main/java/com/insurance/services/CodeLookupService.java
@@ -0,0 +1,27 @@
+/**
+ * 
+ */
+package com.insurance.services;
+
+import com.insurance.domain.InsuranceCodeContainer;
+
+/**
+ * @author francisphiri
+ *
+ */
+public interface CodeLookupService {
+	InsuranceCodeContainer findByISODescription(String text);
+	InsuranceCodeContainer findByNAICS(String text);
+	InsuranceCodeContainer findByGeneralDescription(String text);
+	InsuranceCodeContainer findByGeneralDescriptionContains(String text);
+	InsuranceCodeContainer findByGeneralDescriptionIgnoreCase(String text);
+	InsuranceCodeContainer findByNCCI(String text);
+	InsuranceCodeContainer findByCaWC(String text);
+	InsuranceCodeContainer findByDeWC(String text);
+	InsuranceCodeContainer findByMiWC(String text);
+	InsuranceCodeContainer findByNyWC(String text);
+	InsuranceCodeContainer findByNjWC(String text);
+	InsuranceCodeContainer findByPaWC(String text);
+	InsuranceCodeContainer findByTxWC(String text);
+	
+}
