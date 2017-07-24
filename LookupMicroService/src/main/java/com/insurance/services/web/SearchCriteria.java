dabb9ce83917c765a6f3a23836ad02f2bd2d1cb0
Francis Phiri <franzparks@gmail.com>
2017-07-24 14:53:52 -0700

    update search

---
+++ LookupMicroService/src/main/java/com/insurance/services/web/SearchCriteria.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.insurance.services.web;
+
+/**
+ * @author francisphiri
+ *
+ */
+public class SearchCriteria {
+	
+	private String codeNumber;
+
+	public String getCodeNumber() {
+		return codeNumber;
+	}
+
+	public void setCodeNumber(String codeNumber) {
+		this.codeNumber = codeNumber;
+	}
+	
+	
+	
+}
