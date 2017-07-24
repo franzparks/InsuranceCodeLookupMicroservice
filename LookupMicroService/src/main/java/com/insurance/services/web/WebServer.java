8573816d81a56da9b3325f5921e9f7fe0ee205ab
Francis Phiri <franzparks@gmail.com>
2017-07-23 22:51:23 -0700

    add webserver

---
+++ LookupMicroService/src/main/java/com/insurance/services/web/WebServer.java
@@ -0,0 +1,73 @@
+/**
+ * 
+ */
+package com.insurance.services.web;
+
+import io.pivotal.microservices.services.web.HomeController;
+import io.pivotal.microservices.services.web.WebAccountsController;
+import io.pivotal.microservices.services.web.WebAccountsService;
+import io.pivotal.microservices.services.web.WebServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ * Lokkup web-server. Works as a microservice client, fetching data from the
+ * Lookup-Service. Uses the Discovery Server (Eureka) to find the microservice.
+ * @author francisphiri
+ *
+ */
+@SpringBootApplication
+@EnableDiscoveryClient
+@ComponentScan(useDefaultFilters = false) // Disable component scanner
+public class WebServer {
+	
+	/**
+	 * URL uses the logical name of lookup-service - upper or lower case,
+	 * doesn't matter.
+	 */
+	public static final String LOOKUP_SERVICE_URL = "http://LOOKUP-SERVICE";
+	
+	public static void main(String[] args) {
+		// Tell server to look for web-server.properties or web-server.yml
+		System.setProperty("spring.config.name", "web-server");
+		SpringApplication.run(WebServer.class, args);
+	}
+	
+	@Bean
+	RestTemplate restTemplate() {
+		return new RestTemplate();
+	}
+	
+
+	/**
+	 * The AccountService encapsulates the interaction with the micro-service.
+	 * 
+	 * @return A new service instance.
+	 */
+	@Bean
+	public WebAccountsService accountsService() {
+		return new WebAccountsService(LOOKUP_SERVICE_URL);
+	}
+
+	/**
+	 * Create the controller, passing it the {@link WebAccountsService} to use.
+	 * 
+	 * @return
+	 */
+	@Bean
+	public WebAccountsController accountsController() {
+		return new WebAccountsController(accountsService());
+	}
+
+	@Bean
+	public HomeController homeController() {
+		return new HomeController();
+	}
+
+	
+}
