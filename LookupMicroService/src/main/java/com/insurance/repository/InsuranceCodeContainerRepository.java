f1944351aaa6a186d0825aff334ad9000501e5e9
Francis Phiri <franzparks@gmail.com>
2017-07-23 21:15:01 -0700

    update repository

--- LookupMicroService/src/main/java/com/insurance/repository/InsuranceCodeContainerRepository.java
+++ LookupMicroService/src/main/java/com/insurance/repository/InsuranceCodeContainerRepository.java
@@ -15,4 +15,16 @@ import com.insurance.model.InsuranceCodeContainer;
  */
 public interface InsuranceCodeContainerRepository extends JpaRepository<InsuranceCodeContainer, Long>{
 	Optional<InsuranceCodeContainer> findByISODescription(String text);
+	Optional<InsuranceCodeContainer> findByNAICS(String text);
+	Optional<InsuranceCodeContainer> findByGeneralDescription(String text);
+	Optional<InsuranceCodeContainer> findByGeneralDescriptionContains(String text);
+	Optional<InsuranceCodeContainer> findByGeneralDescriptionIgnoreCase(String text);
+	Optional<InsuranceCodeContainer> findByNCCI(String text);
+	Optional<InsuranceCodeContainer> findByCaWC(String text);
+	Optional<InsuranceCodeContainer> findByDeWC(String text);
+	Optional<InsuranceCodeContainer> findByMiWC(String text);
+	Optional<InsuranceCodeContainer> findByNyWC(String text);
+	Optional<InsuranceCodeContainer> findByNjWC(String text);
+	Optional<InsuranceCodeContainer> findByPaWC(String text);
+	Optional<InsuranceCodeContainer> findByTxWC(String text);
 }
