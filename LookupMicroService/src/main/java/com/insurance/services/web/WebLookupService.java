631f4ff1765bb3a02352f48fee184d0e5f93e2ba
Francis Phiri <franzparks@gmail.com>
2017-07-24 11:06:28 -0700

    clean up

--- LookupMicroService/src/main/java/com/insurance/services/web/WebLookupService.java
+++ LookupMicroService/src/main/java/com/insurance/services/web/WebLookupService.java
@@ -3,18 +3,13 @@
  */
 package com.insurance.services.web;
 
-import java.util.Arrays;
-import java.util.List;
 import java.util.logging.Logger;
 
 import javax.annotation.PostConstruct;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
-import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 /**
@@ -22,9 +17,8 @@ import org.springframework.web.client.RestTemplate;
  * @author francisphiri
  *
  */
-//@Configuration
+
 @Service
-//@Component
 public class WebLookupService {
 	
 	@Autowired
@@ -35,12 +29,6 @@ public class WebLookupService {
 
 	protected Logger logger = Logger.getLogger(WebLookupService.class
 			.getName());
-
-	//@Autowired
-	/*public WebLookupService(String serviceUrl) {
-		this.serviceUrl = serviceUrl.startsWith("http") ? serviceUrl
-				: "http://" + serviceUrl;
-	}*/
 
 	/**
 	 * The RestTemplate works because it uses a custom request-factory that uses
